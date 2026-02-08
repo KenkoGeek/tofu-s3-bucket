364d66bd6d75f2a54f36435db6dcf160a19df795
Franklin G. Mendoza <franklin.g.mendoza@gmail.com>
2026-02-08 14:41:51 -0400

    Add AWS provider and S3 bucket resource

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name       = var.bucket_name
+    ManagedBy = "opentofu"
+    Project   = "brainy"
+  }
+}
